6c0b175f5140a6ab80e1226842b88dca223f3231
gavin.how <gavinhow@live.co.uk>
2023-04-25 09:16:43 +0100

    ci: add logic app workflow

--- infrastructure/modules/logic_app/logic_app.tf
+++ infrastructure/modules/logic_app/logic_app.tf
@@ -1,3 +1,9 @@
+provider "azurerm" {
+  alias           = "logic"
+  # Configuration options
+  features {}
+  subscription_id = var.subscription_id
+}
 
 ###
 ### Logic App
@@ -7,6 +13,7 @@ resource "azurerm_logic_app_workflow" "import_logic_app_workflow" {
   name                = "spoti-logic-import-trigger-${var.environment}"
   resource_group_name = var.resource_group_name
   location            = var.location
+  provider            = azurerm.logic
 }
 
 ###
@@ -16,6 +23,7 @@ resource "azurerm_logic_app_workflow" "import_logic_app_workflow" {
 data "azurerm_managed_api" "postgres" {
   name     = "postgresql"
   location = var.location
+  provider = azurerm.logic
 }
 
 
@@ -24,8 +32,8 @@ resource "azurerm_api_connection" "postgres_connection" {
   resource_group_name = var.resource_group_name
   managed_api_id      = data.azurerm_managed_api.postgres.id
   display_name        = "Spotitrack DB Connection"
-
-  parameter_values = {
+  provider            = azurerm.logic
+  parameter_values    = {
     "encryptConnection" = false,
     "server"            = var.database.host,
     "database"          = var.database.name,
@@ -39,4 +47,12 @@ resource "azurerm_api_connection" "postgres_connection" {
     # NOTE: since the connectionString is a secure value it's not returned from the API
     ignore_changes = [parameter_values]
   }
+}
+
+output "logic_app_resource_group_name" {
+  value = azurerm_logic_app_workflow.import_logic_app_workflow.resource_group_name
+}
+
+output "database_connection_name" {
+  value = azurerm_api_connection.postgres_connection.name
 }
